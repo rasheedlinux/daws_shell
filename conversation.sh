0384e8721df84c8f87840e69234a90a1ac0579d6
hareesh <hareesh@tpconnects.com>
2024-08-24 08:50:45 +0530

    added conversation file to the repo

---
+++ conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh : hello suresh, How are you ?"
+echo "suresh : hello Ramesh i am doing good thankyou and how are you ?"
+echo "ramesh : i am doing great suresh meat me when you are free"
+echo "suresh : sure i will meet you once free"
