fd8c0e3b827057a13d5e70db688e1403b20a0bcc
hareesh <hareesh@tpconnects.com>
2024-08-25 20:23:38 +0530

    passing veriable while executing script

---
+++ passveriable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=$1
+person2=$2
+echo "$person1 : hello $person2"
+echo "${person2}: hello ${person1}, how are you"
+echo "$person1: i am dooing good $person2, what are you doing"
+echo "$person2 i am doing learning shell script"
