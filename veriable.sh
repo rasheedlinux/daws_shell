29bfbe001b5e191b32286a9d2795705bad599c3f
hareesh <hareesh@tpconnects.com>
2024-08-25 19:48:45 +0530

    veriable.sh

---
+++ veriable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+A=Ramesh
+B=suresh
+
+echo " $Ramesh : hello suresh, How are you ?"
+echo " $suresh : hello Ramesh i am doing good thankyou and how are you ?"
+echo " $Ramesh : i am doing great suresh meat me when you are free"
+echo "$suresh : sure i will meet you once free"
