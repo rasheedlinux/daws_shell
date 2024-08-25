9688135c3258bb3113a4a4ec4e756a6c8ab2bb81
hareesh <hareesh@tpconnects.com>
2024-08-25 19:57:34 +0530

    added flower bracket

--- veriable.sh
+++ veriable.sh
@@ -2,7 +2,7 @@
 Ramesh=A
 suresh=B
 
-echo " $Ramesh : hello suresh, How are you ?"
-echo " $suresh : hello Ramesh i am doing good thankyou and how are you ?"
+echo " $Ramesh : hello $suresh, How are you ?"
+echo " $suresh : hello {$Ramesh} i am doing good thankyou and how are you ?"
 echo " $Ramesh : i am doing great suresh meat me when you are free"
 echo " $suresh : sure i will meet you once free"
