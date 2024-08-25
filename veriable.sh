3785e55fc72561a7c6d1260f246677ecf9bf6440
hareesh <hareesh@tpconnects.com>
2024-08-25 19:53:51 +0530

    modified lines in veriables file

--- veriable.sh
+++ veriable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-A=Ramesh
-B=suresh
+Ramesh=A
+suresh=B
 
 echo " $Ramesh : hello suresh, How are you ?"
 echo " $suresh : hello Ramesh i am doing good thankyou and how are you ?"
 echo " $Ramesh : i am doing great suresh meat me when you are free"
-echo "$suresh : sure i will meet you once free"
+echo " $suresh : sure i will meet you once free"
